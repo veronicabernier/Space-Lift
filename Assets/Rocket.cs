cdd77f2fa30779b33ad36397a7c6187b5d864a1a
Veronica Bernier <veronica.bernier@upr.edu>
2020-07-27 19:38:42 -0400

    Added thrusting and rotation

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       ProcessInput(); 
+        ProcessInput(); 
     }
 
     private void ProcessInput() {
         if(Input.GetKey(KeyCode.Space)) {
-            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
         if(Input.GetKey(KeyCode.A)) {
-            print("Rotating left");
+            transform.Rotate(Vector3.forward);
         }
         else if(Input.GetKey(KeyCode.D)) {
-            print("Rotating right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
